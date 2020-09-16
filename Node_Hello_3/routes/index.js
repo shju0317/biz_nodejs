ed0d7e9c87e9b69135aaec5b46df67ed3dd56178
shju0317 <shju0317@naver.com>
2020-09-16 16:35:13 +0900

    200916

--- Node_Hello_3/routes/index.js
+++ Node_Hello_3/routes/index.js
@@ -3,10 +3,18 @@ var router = express.Router();
 
 const moment = require("moment");
 
+router.post("/input", function (req, res) {
+  let user = req.body.user;
+  res.render("index", {
+    user: user,
+    title: "님 반갑습니다",
+  });
+});
+
 router.get("/", function (req, res, next) {
   let date = moment(new Date()).format("YYYY-MM-DD");
   let time = moment(new Date()).format("HH:mm:ss");
-  res.render("index", { title: "반갑습니다", date: date, time: time });
+  res.render("index", { date: date, time: time });
 });
 
 module.exports = router;
